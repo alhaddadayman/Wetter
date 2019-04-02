301a6f972096e1c85a8524aab935e6892ffb176e
alhaddadayman <alhaddad.ay@gmail.com>
2019-04-02 15:12:18 +0200

    test

---
+++ app.js
@@ -0,0 +1,38 @@
+$('#hide').hide();
+$('#hide1').hide();
+$("#btn").click(() => {
+    
+ var city = $('#cityInput').val();
+console.log(city);
+let appId = "&APPID=d2c88add7b631eb9d5c34c5c48de6565";
+let units = "&units=metric";
+$.ajax({
+    //JSON URL
+    url: 'http://api.openweathermap.org/data/2.5/weather?q='+city+appId+units,
+    // type of request ('GET' or 'POSt')
+    type: "GET",
+    // json or html or xml
+    dataType: "json",
+    success: data => {
+      convertHTML(data);
+    },
+    error: error => {
+      console.log('There is an error'+ error);
+    }
+
+  });
+  $('#hide').show();
+  $('#hide1').show();
+  function convertHTML(data) {
+    console.log(data);
+let cityName = city.toUpperCase()+ ', '+data.sys.country +' : ' + data.main.temp + '<sup>°C</sup>' ;
+let wind = data.wind.speed+ ' km/h';
+let humidity = data.main.humidity;
+let description = data.weather[0].description;
+
+    $('#cityNameId').html(cityName);
+    $('#windId').html(wind);
+    $('#humId').html(humidity);
+    $('#desId').html(description); 
+  }
+});
